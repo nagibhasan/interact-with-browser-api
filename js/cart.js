608f576056eb8d28896194c32ee425d8a64ca270
nagibhasan <nagibhasan132@gmail.com>
2023-03-12 12:08:54 +0600

    last commit

--- js/cart.js
+++ js/cart.js
@@ -34,3 +34,14 @@ const saveProductToLocalStorage = (product, quantity) =>{
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 }
+
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getstoredShoppingCart();
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
